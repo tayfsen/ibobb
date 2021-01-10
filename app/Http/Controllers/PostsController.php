e47032292c59ec5934c8f35a4aae52480335afda
Tayfun Sen <sen.ghost@outlook.com>
2021-01-10 21:01:34 +0100

    Projekt-Start

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use DB;
+use App\Models\Post;
+
+class PostsController extends Controller
+{
+
+    public function show($slug)
+    {
+
+            $post = Post::where('slug', $slug)->firstOrFail();
+
+
+
+            return view('post', [
+                'post' => $post
+            ]);
+
+    }
+}
---
+++ resources/views/post.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta
+        name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    >
+    <meta
+        http-equiv="X-UA-Compatible"
+        content="ie-edge"
+    >
+
+    <title>Document</title>
+
+
+
+</head>
+<body>
+<h1>My Blog Post</h1>
+
+<p>{{ $post->body}}</p>
+</body>
+
+</html>
